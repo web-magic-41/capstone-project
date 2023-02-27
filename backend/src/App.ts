4376b9ad201c4b711400de06079710eae66e59b0
Elise Wantling <ewantling@cnm.edu>
2023-02-27 14:33:43 -0700

    added sign out route

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -7,6 +7,7 @@ import { createClient } from 'redis'
 import RedisConnect from 'connect-redis'
 import {signupRoute} from './apis/sign-up/signup.route'
 import{signinRoute} from './apis/sign-in/signin.route'
+import {signOutRoute} from "./apis/sign-out/sign-out.route";
 const redisClient = createClient({ legacyMode: true, socket: { host: process.env.REDIS_HOST } })
 redisClient.connect().catch(console.error)
 const RedisStore = RedisConnect(session)
@@ -49,6 +50,7 @@ export class App {
         this.app.use('/apis', indexRoute)
         this.app.use('/apis/sign-up', signupRoute)
         this.app.use('/apis/sign-in', signinRoute)
+        this.app.use('/apis/sign-out', signOutRoute)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
