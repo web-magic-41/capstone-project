e36d3929f7af13103afb1ccc253650fcbafadeb6
Elise Wantling <ewantling@cnm.edu>
2023-02-27 14:00:10 -0700

    added rating feature

--- backend/src/utils/models/rating.ts
+++ backend/src/utils/models/rating.ts
@@ -1,10 +1,26 @@
+import {sql} from "../database.utils";
 
 export interface Rating {
-    ratingProfileId:
-    rating
+    ratingProfileId: string
+    ratedProfile: string
+    ratingComment: string
+    ratingStarValue: string
 }
 
-rating_profile uuid not null,
-    rated_profile uuid not null,
-    rating_comment varchar(1000),
-    rating_star_value varchar(5),
+export async function insertRating (rating: Rating): Promise<string> {
+    const {ratingProfileId, ratedProfileId} = rating
+    await sql `INSERT INTO "rating" (rating_profile_id, rated_profile_id, rating_comment, rating_star_value) VALUES(${ratingProfileId}, $(ratedProfileId}, NOW())`
+    return "Rating created"
+}
+
+export async function updateRatingStarValue (rating: Rating): Promsie<string> {
+    const {ratingProfileId, ratedProfileId} = rating
+    await sql `UPDATE rating SET ratingStarValue= ${ratingStarValue} WHERE ProfileId=${ProfileId}}`
+    return "Rating star value updated"
+}
+
+export async function updateRatingComment (rating: Rating): Promsie<string> {
+    const {ratingProfileId, ratedProfileId} = rating
+    await sql`UPDATE rating SET ratingComment= ${ratingComment} WHERE ProfileId = ${ProfileId}}`
+    return "Rating comment updated"
+}
