016c795476989e152ff7c691df4290c6432984a0
donavan padilla <dpadilla215@cnm.edu>
2023-03-16 14:02:08 -0600

    fixed merge conflict

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,12 @@
 import listings from "./listings.js";
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import profiles from "./profiles.js";
 import cards from "./cards.js";
-
-const reducer = combineReducers({listings, profiles, cards})
-=======
 import auth from "./auth.js";
 
-const reducer = combineReducers({listings:listings, auth:auth})
->>>>>>> develop
+
+const reducer = combineReducers({listings, profiles, cards, auth:auth})
+
 
 
 export const store = configureStore({reducer})
